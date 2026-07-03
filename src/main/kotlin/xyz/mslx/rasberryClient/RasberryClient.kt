e8db8dcdf4bd965de93ed05467f65d1a4a3a46c8
mslx <mslx567@gmail.com>
2026-07-04 02:39:29 +0330

    Initial commit

---
+++ src/main/kotlin/xyz/mslx/rasberryClient/RasberryClient.kt
@@ -0,0 +1,14 @@
+package xyz.mslx.rasberryClient
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RasberryClient : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
